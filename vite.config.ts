1b02afe6ea4d08cdad795b626ac3f315da580db4
ChristopherLDC <147751717+christopherldc13@users.noreply.github.com>
2026-05-19 08:37:12 -0400

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': {
+        target: 'http://localhost:5000',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 })
