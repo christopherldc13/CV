de813d558500ac017c1fc78c7ddbefe78800f740
ChristopherLDC <147751717+christopherldc13@users.noreply.github.com>
2026-05-19 09:00:19 -0400

    .

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -48,7 +48,7 @@ router.post(
       });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ message: 'Server error.' });
     }
   }
 );
@@ -87,7 +87,7 @@ router.post(
       });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ message: 'Server error.' });
     }
   }
 );
